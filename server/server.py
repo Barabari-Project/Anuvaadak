123ddc4a6ce8e4a4ab8cfcd76887b049fe313680
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2022-11-22 18:27:25 +0530

    Expts more

--- server/server.py
+++ server/server.py
@@ -15,7 +15,7 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
 # Create an object of the above class
 handler_object = MyHttpRequestHandler
 
-PORT = 8000
+PORT = 3000
 my_server = socketserver.TCPServer(("", PORT), handler_object)
 
 # Star the server
