c0c885e6c2b3a3fe5ac223e611f74e829aa8fb93
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2022-11-24 02:52:52 +0530

    Add spawn

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,24 @@
 // https://github.com/lau1944/bunrest
 import type { BunRequest, BunResponse } from "./index.d.ts"
+import { spawnSync } from 'bun';
 import server from "bunrest";
 
 const app = server();
 const PORT = process.env.port || 3000;
+
+
+// const {stdout} = spawnSync(['echo', 'hi']);
+// const text = stdout.toString();
+
+// import {spawn, file, write} from 'bun';
+// await write('/tmp/foo.txt', 'hi');
+// const {stdout} = spawn(['cat'], {
+//   // Set /tmp/foo.txt as stdin
+//   stdin: file('/tmp/foo.txt'),
+// });
+// const text = await new Response(stdout).text();
+// console.log(text); // "hi\n"
+
 
 app.get('/test', (req: BunRequest, res: BunResponse) => {
     res.status(200).json({ message: req.query });
