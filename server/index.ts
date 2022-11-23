32b3a5fd503734789f18f5f0ceca4bacbe45d9b2
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2022-11-24 02:41:00 +0530

    to ts

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,20 @@
+// https://github.com/lau1944/bunrest
+import type { BunRequest, BunResponse } from "./index.d.ts"
 import server from "bunrest";
-const app = server();
 
-app.get('/test', (req, res) => {
+const app = server();
+const PORT = process.env.port || 3000;
+
+app.get('/test', (req: BunRequest, res: BunResponse) => {
     res.status(200).json({ message: req.query });
 });
 
-app.put('/test/:id', (req, res) => {
-    res.status(200).json({ message: req.params.id });
+app.put('/test/:id', (req: BunRequest, res: BunResponse) => {
+    res.status(200).json({ message: req.params?.id });
 });
 
-app.post('/test/:id/:name', (req, res) => {
+app.post('/test/:id/:name', (req: BunRequest, res: BunResponse) => {
     res.status(200).json({ message: req.params });
 });
+
+app.listen(PORT, () => console.log('App is listening on port ' + PORT));
